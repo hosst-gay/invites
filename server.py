06269526311704ea06a98eb8b0b68502c1b4572c
SawshaDev <contactcallimarie@gmail.com>
2022-05-01 23:15:25 +0000

    added index.html

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 from pprint import pprint
-from quart import Quart, jsonify, request
+from quart import Quart, jsonify, request, render_template
+import quart.flask_patch
 import secrets
 
 app = Quart(__name__)
+
+
+@app.route('/')
+async def home():
+    return await render_template("index.html")
 
 
 @app.route('/gentoken')
 async def tokengen():
     token = secrets.token_urlsafe(30)
     print(token)
-    return jsonify(invite=token)
+    return jsonify(invite=token, status=200)
 
 
 app.run(port=5123, debug=True)
