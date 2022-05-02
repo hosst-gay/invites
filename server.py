dd72bdc037d1861a7a96429c1d2b3f3fbd06ad6b
SawshaDev <contactcallimarie@gmail.com>
2022-05-02 04:24:33 +0000

    add database stuff so tokens go to database

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from pprint import pprint
 from quart import Quart, jsonify, request, render_template
 import quart.flask_patch
+from flask_sqlalchemy import SQLAlchemy
 import secrets
 
 app = Quart(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/hosst/website/schema/invites.db'
+
+class invites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invites = db.Column(db.String(100), nullable=False)
 
 
 @app.route('/')
@@ -15,6 +22,14 @@ async def home():
 async def tokengen():
     token = secrets.token_urlsafe(30)
     print(token)
+    try:
+        invite = invites(invites=token)
+        
+        db.session.add(invite)
+
+        db.session.commit()
+    except Exception as e:
+        print(e)
     return jsonify(invite=token, status=200)
 
 
